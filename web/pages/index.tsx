65910b802dc34865ed659b181229a3dc7146fa1f
montblanc0141 <mh5.4wasugi10comaru@gmail.com>
2023-06-23 17:38:49 +0900

    add ohgiri page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -4,14 +4,15 @@ import React, { useCallback, useState } from 'react'
 import styles from '../styles/Home.module.css'
 import { db } from '../libs/firebase'
 import { addDoc, collection, doc, setDoc } from 'firebase/firestore'
-import { getStorage, ref, uploadBytes } from "firebase/storage"
+import { getStorage, ref, uploadBytes } from 'firebase/storage'
 import { Box, Button, Center, Flex, IconButton, Spinner, Tooltip, useToast } from '@chakra-ui/react'
 import FileInput from '../components/file-input'
 import { getRandomStr } from '../libs/random'
 import TextArea from '../components/textarea'
 import { AddIcon, CopyIcon } from '@chakra-ui/icons'
+import Link from 'next/link'
 
-const ASSET_FOLDER_NAME = "assets"
+const ASSET_FOLDER_NAME = 'assets'
 
 type News = {
   content: string
@@ -20,36 +21,35 @@ type News = {
 }
 
 const Home: NextPage = () => {
-  const [contents, setContents] = useState<string[]>([""])
+  const [contents, setContents] = useState<string[]>([''])
   const [createdBy, setCreatedBy] = useState('川元')
   const [isLoading, setLoading] = useState(false)
-  const storage = getStorage();
+  const storage = getStorage()
   const toast = useToast()
 
   const addContent = useCallback(() => {
-    setContents(old => [...old, ''])
+    setContents((old) => [...old, ''])
   }, [])
-
 
   const postNews = async () => {
     setLoading(true)
     try {
-      const news: News[] = contents.map(c => ({content: c, createdBy, createdAt: new Date()}))
+      const news: News[] = contents.map((c) => ({ content: c, createdBy, createdAt: new Date() }))
       const colRef = collection(db, 'colNews')
-      await Promise.all([news.map(async (n:News) => await addDoc(colRef, n))])
+      await Promise.all([news.map(async (n: News) => await addDoc(colRef, n))])
       toast({
-        title: "近況を登録しました！",
-        status: "success",
-        duration:3000,
-        isClosable: true
+        title: '近況を登録しました！',
+        status: 'success',
+        duration: 3000,
+        isClosable: true,
       })
       setContents([''])
     } catch (err: unknown) {
       toast({
-        title: "エラーが発生しました",
+        title: 'エラーが発生しました',
         status: 'error',
-        duration:3000,
-        isClosable: true
+        duration: 3000,
+        isClosable: true,
       })
     } finally {
       setLoading(false)
@@ -58,7 +58,7 @@ const Home: NextPage = () => {
 
   const getChangeFunc = (i: number) => {
     return (val: string) => {
-      setContents((old)=> old.map((c, index) => (index === i ? val : c)))
+      setContents((old) => old.map((c, index) => (index === i ? val : c)))
     }
   }
 
@@ -67,37 +67,45 @@ const Home: NextPage = () => {
   }
 
   const getPasteClipboardFunc = (i: number) => {
-    return async() => {
-    const text = await navigator.clipboard.readText()
-    const changeFunc = getChangeFunc(i)
-    changeFunc(text)
+    return async () => {
+      const text = await navigator.clipboard.readText()
+      const changeFunc = getChangeFunc(i)
+      changeFunc(text)
+    }
   }
-}
 
-  const handleUploadFile = useCallback(async(file: File) => {
-    const randomStr = getRandomStr();
-    const fileName =  randomStr + file.name;
-    const type = file.type
-    const metaData = {
-      contentType: type
-    }
-    const assetsRef = ref(storage, `${ASSET_FOLDER_NAME}/${fileName}`)
-
-    try{
-      const data = await uploadBytes(assetsRef, file, metaData);
-      const fileData = {
-        createdBy: createdBy,
-        createdAt: data.metadata.timeCreated,
-        mineType: data.metadata.contentType,
-        url: data.metadata.fullPath
+  const handleUploadFile = useCallback(
+    async (file: File) => {
+      const randomStr = getRandomStr()
+      const fileName = randomStr + file.name
+      const type = file.type
+      const metaData = {
+        contentType: type,
       }
-      const ref = doc(collection(db, 'files'))
-      await setDoc(ref, fileData)
-      toast({title: "ファイルアップデート成功", status: "success", duration: 3000, isClosable: true})
-    }catch{
-      toast({title: "Failed to upload file", status: "error", duration: 3000, isClosable:true })
-    }
-  }, [createdBy, storage, toast])
+      const assetsRef = ref(storage, `${ASSET_FOLDER_NAME}/${fileName}`)
+
+      try {
+        const data = await uploadBytes(assetsRef, file, metaData)
+        const fileData = {
+          createdBy: createdBy,
+          createdAt: data.metadata.timeCreated,
+          mineType: data.metadata.contentType,
+          url: data.metadata.fullPath,
+        }
+        const ref = doc(collection(db, 'files'))
+        await setDoc(ref, fileData)
+        toast({
+          title: 'ファイルアップデート成功',
+          status: 'success',
+          duration: 3000,
+          isClosable: true,
+        })
+      } catch {
+        toast({ title: 'Failed to upload file', status: 'error', duration: 3000, isClosable: true })
+      }
+    },
+    [createdBy, storage, toast],
+  )
 
   return isLoading ? (
     <Center>
@@ -113,24 +121,31 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>近況教えて！</h1>
-        <Box >
-        {
-          contents.map((c, i) => {
+        <Box>
+          {contents.map((c, i) => {
             return (
               <Box key={i} m={3}>
-                <Flex justifyContent="center" gap={5} alignItems="center">
-                <TextArea  onChange={getChangeFunc(i)} value={contents[i]}/>
-                <Tooltip label="クリップボードからコピー">
-                <IconButton aria-label="paste clipboard" icon={<CopyIcon/>} onClick={getPasteClipboardFunc(i)}/>
-                </Tooltip>
+                <Flex justifyContent='center' gap={5} alignItems='center'>
+                  <TextArea
+                    onChange={getChangeFunc(i)}
+                    value={contents[i]}
+                    placeholder={'今週なにしたんだい？'}
+                  />
+                  <Tooltip label='クリップボードからコピー'>
+                    <IconButton
+                      aria-label='paste clipboard'
+                      icon={<CopyIcon />}
+                      onClick={getPasteClipboardFunc(i)}
+                    />
+                  </Tooltip>
                 </Flex>
-                </Box>)
-          })
-        }
+              </Box>
+            )
+          })}
         </Box>
-                <Tooltip label="新規コンテンツ">
-        <IconButton aria-label='add textarea' icon={<AddIcon/>} onClick={addContent}/>
-                </Tooltip>
+        <Tooltip label='新規コンテンツ'>
+          <IconButton aria-label='add textarea' icon={<AddIcon />} onClick={addContent} />
+        </Tooltip>
         <select className={styles.card} onChange={handleChangeSelect}>
           <option value='川元'>川元</option>
           <option value='馬場'>馬場</option>
@@ -139,8 +154,11 @@ const Home: NextPage = () => {
         </select>
         <Button onClick={postNews}>近況を登録！！</Button>
         <Box m={3}>
-        <FileInput onSend={handleUploadFile}/>
+          <FileInput onSend={handleUploadFile} />
         </Box>
+        <Link href='/ohgiri'>
+          <a>今週の大喜利はこちら</a>
+        </Link>
       </main>
       <footer className={styles.footer}></footer>
     </div>
---
+++ web/pages/ohgiri/index.tsx
@@ -0,0 +1,123 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import React, { useEffect, useState } from 'react'
+import styles from '../../styles/Home.module.css'
+import { db } from '../../libs/firebase'
+import {
+  DocumentData,
+  collection,
+  doc,
+  getDocs,
+  getFirestore,
+  query,
+  setDoc,
+  where,
+} from 'firebase/firestore'
+import { Box, Button, Center, Flex, Link, Spinner, useToast } from '@chakra-ui/react'
+import TextArea from '../../components/textarea'
+
+type Ohgiri = {
+  question: string
+  status: string
+  createdAt: Date
+  answers: Answer[]
+}
+
+type Answer = {
+  content: string
+  score?: number
+  createdBy: string
+  createdAt: Date
+}
+
+const Ohgiri: NextPage = () => {
+  const [ohgiriId, setOhgiriId] = useState('')
+  const [ohgiri, setOhgiri] = useState<DocumentData | null>(null)
+  const [content, setContent] = useState('')
+  const [createdBy, setCreatedBy] = useState('川元')
+  const [isLoading, setLoading] = useState(false)
+  const toast = useToast()
+
+  const postAnswer = async () => {
+    setLoading(true)
+    try {
+      ohgiri?.answers.push({ content, createdBy, createdAt: new Date(), score: 0 })
+      const docRef = doc(db, 'ohgiri', ohgiriId)
+      await setDoc(docRef, ohgiri)
+      toast({
+        title: '回答を登録しました！',
+        status: 'success',
+        duration: 3000,
+        isClosable: true,
+      })
+      setContent('')
+    } catch (err: unknown) {
+      toast({
+        title: 'エラーが発生しました',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const getChangeFunc = (value: string) => {
+    setContent(value)
+  }
+
+  const handleChangeSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCreatedBy(e.target.value)
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const q = query(collection(db, 'ohgiri'), where('status', '==', 'waiting'))
+      const querySnapshot = await getDocs(q)
+
+      querySnapshot.forEach((doc) => {
+        setOhgiriId(doc.id)
+        setOhgiri(doc.data())
+      })
+    }
+    fetchData()
+  }, [])
+
+  return isLoading ? (
+    <Center>
+      <Spinner />
+    </Center>
+  ) : (
+    <div className={styles.container}>
+      <Head>
+        <title>Catch App</title>
+        <meta name='description' content='Share your life update!' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>今週の大喜利！</h1>
+        <p className={styles.description}>お題：{ohgiri?.question}</p>
+        <Box m={3}>
+          <Flex justifyContent='center' gap={5} alignItems='center'>
+            <TextArea onChange={getChangeFunc} value={content} placeholder={'面白い答えをここに'} />
+          </Flex>
+        </Box>
+        <select className={styles.card} onChange={handleChangeSelect}>
+          <option value='川元'>川元</option>
+          <option value='馬場'>馬場</option>
+          <option value='雷鳥'>雷鳥</option>
+          <option value='鈴木'>鈴木</option>
+        </select>
+        <Button onClick={postAnswer}>回答を登録！！</Button>
+        <Link href='/' className={styles.link}>
+          <a>近況登録はこちら</a>
+        </Link>
+      </main>
+      <footer className={styles.footer}></footer>
+    </div>
+  )
+}
+
+export default Ohgiri
