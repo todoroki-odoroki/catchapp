b0c7d0fbfb1a1a4416abe146508427d58bce6705
montblanc0141 <mh5.4wasugi10comaru@gmail.com>
2023-11-18 21:45:16 +0900

    fix ci

--- functions/src/funcs/post_reminder_to_line.ts
+++ functions/src/funcs/post_reminder_to_line.ts
@@ -8,8 +8,8 @@ dotenv.config();
 
 // Initialize LINE
 const lineConfig: LineConfig = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN ?? "",
-  channelSecret: process.env.CHANNEL_SECRET ?? "",
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN ?? "",
+  channelSecret: process.env.LINE_CHANNEL_SECRET ?? "",
 };
 const client = new line.Client(lineConfig);
 
